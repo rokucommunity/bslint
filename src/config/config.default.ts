3f20529866106aaac76ab5d45145bb35c2a133fd
Charlie Abbott <106152156+charlie-abbott-deltatre@users.noreply.github.com>
2024-03-12 21:12:24 +0000

    Adding default config logic - to support defining broadcast safe color definitions

---
+++ src/config/config.default.ts
@@ -0,0 +1,12 @@
+import { Config } from './configTypes';
+
+const defaultConfig: Config = {
+    colors: {
+        broadcastSafe: {
+            black: '161616',
+            white: 'EBEBEB'
+        }
+    }
+};
+
+export default defaultConfig;
