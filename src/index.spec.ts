7e188cfc4d704075b256be5608a98d6316ef7126
Bart van den Ende <bart.van.den.ende@wbd.com>
2024-05-16 09:27:07 +0200

    chore: update schema to draft-07

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,14 +1,16 @@
+import Ajv from 'ajv';
 import { expect } from 'chai';
-import { Validator } from 'jsonschema';
 import type { BsLintConfig } from '.';
 import { BsLintJsonAllRules } from './__fixture__/bslint.json';
 import BsLintSchema from './schema/bslint.schema.json';
 
 describe('index', () => {
+    const validator = new Ajv().compile(BsLintSchema);
+
     it('Validate if bslint.json fixture matches json-schema and types', () => {
         const config: BsLintConfig = BsLintJsonAllRules;
-        const validator = new Validator();
-        const result = validator.validate(config, BsLintSchema);
-        expect(result.valid).equal(true);
+        const result = validator(config);
+        expect(validator.errors).equal(null);
+        expect(result).equal(true);
     });
 });
