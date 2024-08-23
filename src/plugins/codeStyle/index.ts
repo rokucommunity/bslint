a33647218b77a0934017f8008a6df61c7298ace1
akovtko <akovtko@roku.com>
2024-08-23 16:25:40 +0300

    array and assocarray interace type are case insensitive

--- src/plugins/codeStyle/index.ts
+++ src/plugins/codeStyle/index.ts
@@ -48,7 +48,7 @@ export default class CodeStyle {
             if (tag.text === 'field') {
                 const typeAttribute = attributes.find(({ key }) => key.text === 'type');
 
-                const typeValue = typeAttribute?.value.text;
+                const typeValue = typeAttribute?.value.text?.toLowerCase();
                 if (typeValue === 'array' && validateArrayComponentFieldType) {
                     diagnostics.push(
                         messages.noArrayFieldType(
