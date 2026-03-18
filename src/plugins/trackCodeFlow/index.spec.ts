1a5a66cb8880b0df9c663bb6cc245e243b43c7bc
Luis Soares <luis.soares@sky.uk>
2026-03-18 22:38:30 +0000

    Add failing test

--- src/plugins/trackCodeFlow/index.spec.ts
+++ src/plugins/trackCodeFlow/index.spec.ts
@@ -372,7 +372,8 @@ describe('trackCodeFlow', () => {
         });
         const actual = fmtDiagnostics(diagnostics);
         const expected = [
-            `01:LINT1006:Parameter 'unusedParam' is set but value is never used`
+            `01:LINT1006:Parameter 'unusedParam' is set but value is never used`,
+            `06:LINT1006:Parameter 'hey' is set but value is never used`
         ];
         expect(actual).deep.equal(expected);
     });
